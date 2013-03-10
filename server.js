3be699d35fae3da292d0d609b27986378db70347
Jon Packer <contact@jonpacker.com>
2013-03-10 21:33:49 +0100

    added stylus and jade infrastructure to server

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var stylus = require('stylus')
+var nib = require('nib');
 var db = require('./db');
 var app = express();
 
+function compileStylus(str, path) {
+  return stylus(str).set('filename', path).use(nib());
+};
+
 db(function(err, db) {
   if (err) throw err;
+
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.logger('dev'));
+  app.use(stylus.middleware({ 
+    src: __dirname + '/public/css/', 
+    compile: compileStylus 
+  }));
+  app.use(express.static(__dirname + '/public'));
 
   require('./routes')(app, db);
   app.listen(5892, function() {
