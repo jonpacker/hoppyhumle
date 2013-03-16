c4bf5bea1f00103f67bfc8269b527050f1ef9797
Jon Packer <contact@jonpacker.com>
2013-03-16 10:47:35 +0100

    Made slightly less configurable

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Hoppyhumle Blog Engine
 
 A minimal blog engine based on neo4j and express.
+
+## Configuration options
+
+```
+{
+  "port": 1234, // Port that the server will run on
+  "logger": "dev", // Log level to use (can be unset/null),
+  "pubdir": __dirname + "/public" // Statically served pub dir
+}
+```
+
+* Format of styling is `stylus` and should be supplied from the `css`
+  dir under `pubdir`. 
--- hoppyhumle.js
+++ hoppyhumle.js
@@ -2,6 +2,7 @@ var express = require('express');
 var stylus = require('stylus')
 var nib = require('nib');
 var db = require('./db');
+var path = require('path');
 var app = express();
 
 function compileStylus(str, path) {
@@ -12,16 +13,16 @@ module.exports = function(config) {
   db(function(err, db) {
     if (err) throw err;
 
-    app.set('views', config.dirs.routes);
+    app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     if (config.logger) app.use(express.logger(config.logger));
     app.use(stylus.middleware({ 
-      src: config.dirs.stylus, 
+      src: path.join(config.pubdir, 'css'), 
       compile: compileStylus 
     }));
-    app.use(express.static(config.dirs.pub));
+    app.use(express.static(config.pubdir));
 
-    require(config.dirs.routes)(app, db);
+    require('./routes')(app, db);
     app.listen(config.port, function() {
       console.log("Listening on", config.port);
     });
